4028e61b62c75b46015c3e5a49cc4ccd6432f074
Tsukuyomi212 <nada.zelic@gmail.com>
2021-10-22 16:17:59 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Navbar } from './components/navbar/Navbar';
+import { Homepage } from './components/homepage/Homepage';
+import { HOMEPAGE, LOGIN, SIGNUP } from './utils/routes';
+import { LoginPage } from './components/login/LoginPage';
+import { SignupPage } from './components/signup/SignupPage';
+import { AuthContextProvider } from './contexts/AuthContextProvider';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <Navbar />
+          <Switch>
+            <Route exact path={HOMEPAGE} component={Homepage} />
+            <Route exact path={SIGNUP} component={SignupPage} />
+            <Route exact path={LOGIN} component={LoginPage} />
+          </Switch>
+        </AuthContextProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
