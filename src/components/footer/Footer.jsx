be481c8884896060c8513d08f8504a4a0523c841
Tsukuyomi212 <nada.zelic@gmail.com>
2021-11-02 12:19:31 +0100

    add app logo

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+export const Footer = () => {
+  return (
+    <div>
+      <div>
+        Icons made by
+        <a href="https://www.flaticon.com/authors/bomsymbols" title="BomSymbols">
+          BomSymbols
+        </a>
+        from
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
+    </div>
+  );
+};
